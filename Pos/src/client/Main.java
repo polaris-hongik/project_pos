6501b70fe68432173130da98178ac458ab36ec9d
MurlocKing <MurlocKing@DESKTOP-IFFMI3P>
2021-01-15 23:03:43 +0900

    카드 결제 추가

--- Pos/src/client/Main.java
+++ Pos/src/client/Main.java
@@ -3,8 +3,9 @@ package client;
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Pos pos = new BookstorePos();
+		pos.setPaymentMethod(new CardPayment());
+		pos.pay();
 	}
 
 }
